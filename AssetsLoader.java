df8fda3b478a34280251f8a7f52b9e3c5a050934
DGh0st <deeppwnage@yahoo.com>
2017-03-09 18:26:31 -0600

    Fixed AssetsLoader's file searching location to start at current dir and added AssetsTest to test runner

--- AssetsLoader.java
+++ AssetsLoader.java
@@ -11,28 +11,28 @@ public class AssetsLoader
     String path;
     switch(id){
       case 0:
-        path = "/resources/textures/SpriteSheet01.png"; //generic location for the sheet
+        path = "./resources/textures/SpriteSheet01.png"; //generic location for the sheet
         break;
       case 1:
-        path = "/resources/textures/SpriteSheet02.png"; //generic location for the sheet
+        path = "./resources/textures/SpriteSheet02.png"; //generic location for the sheet
         break;
       case 2:
-        path = "/resources/textures/SpriteSheet03.png"; //generic location for the sheet
+        path = "./resources/textures/SpriteSheet03.png"; //generic location for the sheet
         break;
       case 3:
-        path = "/resources/textures/SpriteSheet04.png"; //generic location for the sheet
+        path = "./resources/textures/SpriteSheet04.png"; //generic location for the sheet
         break;
       case 4:
-        path = "/resources/textures/SpriteSheet05.png"; //generic location for the sheet
+        path = "./resources/textures/SpriteSheet05.png"; //generic location for the sheet
         break;
       case 5:
-        path = "/resources/textures/SpriteSheet06.png"; //generic location for the sheet
+        path = "./resources/textures/SpriteSheet06.png"; //generic location for the sheet
         break;
       case 6:
-        path = "/resources/textures/SpriteSheet07.png"; //generic location for the sheet
+        path = "./resources/textures/SpriteSheet07.png"; //generic location for the sheet
         break;
       default:
-        path = "/resources/textures/SpriteSheet01.png"; //placeholder as default
+        path = "./resources/textures/SpriteSheet01.png"; //placeholder as default
     }
     return path;
   }
