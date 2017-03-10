7c54ec9b2ad3a622bb16b4dd96a5a86243a39e34
DGh0st <deeppwnage@yahoo.com>
2017-03-09 18:32:00 -0600

    Setup the basic CollisionEvent/Listener, implementation not complete yet

---
+++ CollisionEvent.java
@@ -0,0 +1,12 @@
+/**
+ * Nerd 03/09/2017
+ * Deep Patel
+ * CollisionEvent class used to check for collisions call observers when collision occurs
+ * TODO: Implement collision check
+ */
+
+public class CollisionEvent {
+	void checkCollision() {
+		// TODO: Implement the collision check
+	}
+}
---
+++ CollisionListener.java
@@ -0,0 +1,9 @@
+/**
+ * Nerd 03/09/2017
+ * Deep Patel
+ * CollisionListener interface class used to listen for collision events
+ */
+
+public interface CollisionListener {
+	void collisionDetected();
+}
