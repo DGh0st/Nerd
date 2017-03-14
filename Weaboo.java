bc9b1903129e3f4d095c0322026e802ec01cddb6
kandyceasania101 <kburks2@uic.edu>
2017-03-14 00:11:03 -0500

    Implemented draw() method.

--- Weaboo.java
+++ Weaboo.java
@@ -6,9 +6,15 @@
  *  - Implement draw() Method
  *  - Implement play() Method
  */
+import java.awt.Graphics;
+
 class Weaboo extends Character{
+  int RECT_X = 10, RECT_Y = 10, RECT_WIDTH = 10, RECT_HEIGHT = 10;
+  
   public Weaboo(int x, int y){
     super(x,y);
+    Graphics g = getGraphics();
+    g.drawRect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT);
   }
   
   public void draw(){
