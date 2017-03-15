cb3f59740bb7d958e2d25c46cce6ef9ff126ffec
DGh0st <deeppwnage@yahoo.com>
2017-03-15 01:10:00 -0500

    Added under construction for settings and shop

--- MainMenu.java
+++ MainMenu.java
@@ -52,20 +52,34 @@ public class MainMenu extends Menu {
 	private void setupShopScreen(Dimension windowSize) {
 		shopScreen = createScreen(windowSize, new FlowLayout());
 
-		addHeader(shopScreen, "Nerd Shop", "mainFromShop", windowSize, 0);
+		addHeader(shopScreen, "Nerd Shop", "mainFromShop", windowSize);
 
 		// TODO: Add characters to buy
+
+		// TODO: remove under construction
+		Font constructionFont = new Font(Font.SERIF, Font.PLAIN, 32);
+		JLabel underConstruction = createLabel("Under Construction", constructionFont);
+		shopScreen.add(Box.createRigidArea(new Dimension(windowSize.width / 2 - (int)(underConstruction.getPreferredSize().getWidth() - 30), 20)));
+		shopScreen.add(underConstruction);
+		shopScreen.add(Box.createRigidArea(new Dimension(windowSize.width / 2 - (int)(underConstruction.getPreferredSize().getWidth() + 30), 20)));
 	}
 
 	private void setupSettingsScreen(Dimension windowSize) {
 		settingsScreen = createScreen(windowSize, new FlowLayout());
 
-		addHeader(settingsScreen, "Settings", "mainFromSet", windowSize, 45);
+		addHeader(settingsScreen, "Settings", "mainFromSet", windowSize);
 
 		// TODO: Add settings
+
+		// TODO: remove under construction
+		Font constructionFont = new Font(Font.SERIF, Font.PLAIN, 32);
+		JLabel underConstruction = createLabel("Under Construction", constructionFont);
+		settingsScreen.add(Box.createRigidArea(new Dimension(windowSize.width / 2 - (int)(underConstruction.getPreferredSize().getWidth() - 30), 20)));
+		settingsScreen.add(underConstruction);
+		settingsScreen.add(Box.createRigidArea(new Dimension(windowSize.width / 2 - (int)(underConstruction.getPreferredSize().getWidth() + 30), 20)));
 	}
 
-	private void addHeader(JPanel panel, String displayTitle, String backButtonCommand, Dimension windowSize, int padding) {
+	private void addHeader(JPanel panel, String displayTitle, String backButtonCommand, Dimension windowSize) {
 		Font titleFont = new Font(Font.SERIF, Font.PLAIN, 80);
 		Font buttonFont = new Font(Font.SERIF, Font.PLAIN, 32);
 		Font backFont = new Font(Font.SERIF, Font.PLAIN, 16);
@@ -74,12 +88,13 @@ public class MainMenu extends Menu {
 		backButton.setPreferredSize(new Dimension(64, 64));
 		panel.add(backButton);
 
-		panel.add(Box.createRigidArea(new Dimension(windowSize.width / 4 + padding, 20)));
-
 		JLabel title = createLabel(displayTitle, titleFont);
+
+		panel.add(Box.createRigidArea(new Dimension(windowSize.width / 2 - (int)(title.getPreferredSize().getWidth() / 2) - (int)(backButton.getPreferredSize().getWidth()), 20)));
+
 		panel.add(title);
 
-		panel.add(Box.createRigidArea(new Dimension(windowSize.width / 4 + padding, 20)));
+		panel.add(Box.createRigidArea(new Dimension(windowSize.width / 2 - (int)(title.getPreferredSize().getWidth() / 2) - (int)(backButton.getPreferredSize().getWidth()), 20)));
 	}
 
 	public void update() {
