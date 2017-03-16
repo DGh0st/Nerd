f2b47bbd91423ae1e9bc594c39b2abd6fccd67af
rhruby2 <rhruby066@gmail.com>
2017-03-15 20:29:47 -0500

    Add files via upload

--- JUnitTests/LocationTest.java
+++ JUnitTests/LocationTest.java
@@ -1,7 +1,7 @@
 /*
  *  ~ NerdTester.java
  * Raymond Hruby II
- * 03/06/2017
+ * 03/15/2017
  * Description: tester to collectively and extensively test each class 
  */
 
@@ -11,35 +11,51 @@ import org.junit.Test;
 import junit.framework.TestCase;
 
 public class LocationTest extends TestCase{
+  
   @Ignore
   @Test
-  //LOCATION | DIMENSION
   public void testLocationGetters(){
-    Location test = new Location("",0,50,60);
-    assertEquals(test.getWidth(),50);
-    assertEquals(test.getHeight(),60);
+    OzLocation test = new OzLocation(0);
+    assertEquals(test.getWidth(),22);
+    assertEquals(test.getHeight(),14);
     assertEquals(test.getId(),0);  
   }
   
   @Ignore
   @Test
+  public void testLocationObstacles(){
+    LocationArray locations = new LocationArray(6); 
+    
+    Car test = new Car();
+    
+    locations.getCurrentLocation().addMovable(test);
+    
+    Car test2 = (Car)locations.getCurrentLocation().getMovable(0);
+    
+    assertEquals(test,test2);
+    //assertNotEquals(locations.getMovable(0),null);
+  }
+  /*
+  @Ignore
+  @Test
   //LOCATION ARRAY | TOTAL | LOCATIONINDEX 
   public void testLocationArray(){
-    LocationArray locations = new LocationArray(5);    
-    assertEquals(locations.getTotal(),5);
-    assertEquals(locations.getLocation(0),null);
+    LocationArray locations = new LocationArray(7);    
+    assertEquals(locations.getTotal(),7);
+    //assertEquals(locations.getCurrentLocation(),null);
   }
   
   @Ignore
   @Test
   //LOCATION ARRAY | TOTAL | LOCATIONINDEX 
   public void testLocationInsertion(){
-    LocationArray locations = new LocationArray(6);    
-    assertEquals(locations.getLocation(0),null);
+    LocationArray locations = new LocationArray(7);    
+    //assertEquals(locations.getCurrentLocation());
     
     //Location test = new Location("",0,50,50);
     //locations.addLocation(test);
     //assertEquals(locations.getLocation(0),test);
   }
+  */
 };                 
                                       
