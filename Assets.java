c2402bbb4b27098e8c8c6d705491007d21df7954
rhruby2 <rhruby066@gmail.com>
2017-04-09 19:31:43 -0500

    added assets for new cars
    for the bigger cars, there is now an ArrayList sprites_cars that holds the car images double the width of tiles (128,64) or (WIDTH*2,HEIGHT).
    
    use getCarSprite(index) to grab image
    
    sprites arraylist is increased to hold the new cars, but the images are split in two, because they are twice the width.

--- Assets.java
+++ Assets.java
@@ -1,59 +1,26 @@
-/**
- * ~ Assets.java
- * Raymond Hruby II
- * 04/03/2017
- * Assets class holds sprite images, currently only worldTile data
- */
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
 
+//assets = images, sound,music
 public class Assets
 {
-  private static final int WIDTH=64, HEIGHT=64, ROWS=5,COLS=5;
+  private static final int WIDTH=16, HEIGHT=16;
+  public static BufferedImage player,enemy,grass_1,grass_2,mud_1,mud_2,stone_1,stone_2;
   
-  private static Assets instance = null;
-  
-  //changed 2d array to 1d array so index acts as id for sprite image
-  //changed 1d array to individual sprites to attach sprites to name
-  //changed back to 1d array so Assets can be only assets class, and will update and use one arrayList
-  public static ArrayList<BufferedImage> sprites;
-  private String path;        //path to Spritesheet 
-
-  public static Assets getInstance() {
-    if (instance == null) {
-      instance = new Assets( LocationArray.getInstance().getCurrentLocationIndex() );
-    }
-    return instance;
+  public static void init(){
+     SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/Tutorial_SpriteSheet_Textured.png"));
+     
+     //first row
+     player = sheet.crop(0    ,0,WIDTH,HEIGHT);
+     enemy = sheet.crop (WIDTH,0,WIDTH,HEIGHT);
+     
+     //second row
+     grass_1 = sheet.crop(0,      HEIGHT,WIDTH,HEIGHT);
+     grass_2 = sheet.crop(WIDTH,  HEIGHT,WIDTH,HEIGHT);
+     mud_1 = sheet.crop  (WIDTH*2,HEIGHT,WIDTH,HEIGHT);
+     mud_2 = sheet.crop  (WIDTH*3,HEIGHT,WIDTH,HEIGHT);
+     
+     //third row
+     stone_1 = sheet.crop(0,    HEIGHT*2,WIDTH,HEIGHT);
+     stone_2 = sheet.crop(WIDTH,HEIGHT*2,WIDTH,HEIGHT);
   }
-  private Assets(int locationId){ //private constructor for Singleton pattern, use getInstance()
-    System.out.println("inpath" + this.path);
-    this.path = AssetsLoader.getAssetsPath(locationId);
-    System.out.println("inpath" + this.path);
-    initialize();
-  }
-  public void initialize(){
-    SpriteSheet sheet = new SpriteSheet(this.path);
-    sprites = new ArrayList<BufferedImage>(ROWS*COLS);
-    
-    for (int i = 0; i < ROWS; i++){
-        for (int j = 0; j < COLS; j++){
-          sprites.add( i*ROWS+j, sheet.crop( j*WIDTH, i*HEIGHT, WIDTH, HEIGHT ) );
-        }
-      }
-  }
-  
-  //Only call if there is a new locaiton
-  public void update(){
-    int newLocationId = LocationArray.getInstance().getCurrentLocationIndex();
-    
-    System.out.println("inpath" + this.path);
-    this.path = AssetsLoader.getAssetsPath(newLocationId);
-    System.out.println("inpath" + this.path);
-    
-    initialize();
-  }
-  public BufferedImage getSprite(int index){
-    return sprites.get(index);
-  }
-
 }
