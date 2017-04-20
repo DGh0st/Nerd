133e04267d54f6d873c55a4f575aafc86e3f012b
kandyceasania101 <kburks2@uic.edu>
2017-04-20 17:39:20 -0500

    Plant Object Classes

---
+++ Plant01.java
@@ -0,0 +1,35 @@
+/**
+ * Nerd 03/11/2017
+ * Kandyce Burks
+ * Tree class that is a subclass StaticObstacle class
+ * TODO:
+ *  - Implement draw() Method
+ *  - Implement play() Method
+ */
+
+import java.awt.Graphics;
+//import java.util.concurrent.ThreadLocalRandom;
+import java.awt.image.BufferedImage;
+
+class Plant01 extends StaticObstacle{
+  int RECT_WIDTH = Tile.TILE_WIDTH, RECT_HEIGHT = Tile.TILE_HEIGHT;
+  BufferedImage image;
+
+  public Plant01(int x, int y) {
+   super(x, y);
+  }
+  public void draw(){
+   draw(GameState.getInstance().getGraphics());
+  }
+  public void draw(Graphics g){
+   int x = position.getX();
+   int y = position.getY();
+    if (g != null) {
+      BufferedImage image = Assets.getInstance().getSprite(39);
+      g.drawImage(image, x*64, y*64, image.getWidth(), image.getHeight(), null);
+    }
+  }
+  public void play(){
+   //TODO
+  }
+}
